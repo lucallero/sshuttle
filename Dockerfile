39279dae83818a325e9e82247a88074090bef681
Luciano Callero <lucianocallero@gmail.com>
2018-09-09 19:10:21 -0300

    Another aproach for keygen.

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,6 @@ RUN apt-get update && \
 
 RUN echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 
-RUN ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -N ''
-
 COPY start.sh ./
 
 CMD ["./start.sh"]
--- start.sh
+++ start.sh
@@ -16,7 +16,47 @@ echo "bye!"
 # Traping signals
 trap _term SIGINT SIGTERM
 
-# Main process
-sshuttle -vr $USER@$HOST -x 10.0.0.0/8 -x 192.168.0.0/16 $SUBNET &
+
+# Checking for mandatories env variables.
+STOP=0
+set -- "USER"  "MODE"  "HOST" "SUBNET"
+for i; do
+    env | grep $i > /dev/null
+    RC=$?
+    if [[ $RC == 1 ]];then
+	echo "Env variable $i was not set, I need it!"
+	STOP=1
+    fi
+done
+
+if [[ $MODE == "genkey" ]];then
+    if [ ! -f /home/${USER}/.ssh/id_rsa ];then
+        echo "User: $USER"
+        mkdir -p /home/${USER}/.ssh
+        ssh-keygen -f /home/${USER}/.ssh/id_rsa -t rsa -N ''                
+        echo "Copy following pub key to your server:"
+        echo "`cat /home/${USER}/.ssh/id_rsa.pub`"
+        echo "Host *
+                StrictHostKeyChecking no
+                UserKnownHostsFile=/dev/null" >> /home/${USER}/.ssh/config
+        exit 0
+    else
+        echo "File exists, /home/${USER}/.ssh/id_rsa.pub, copy to your server and run with MODE=connect"
+        exit 0
+    fi  
+elif [[ $RUN_MODE == "connect" ]];then
+    # Main process
+    env | grep PORT > /dev/null
+    RC=$?
+    if [ $# -eq 0 ] && [ $RC == 1 ];then    
+        #if no port specified use default
+        sshuttle -vr $USER@$HOST -x 10.0.0.0/8 -x 192.168.0.0/16 $SUBNET &    
+    else
+        sshuttle -vr $USER@$HOST:$PORT -x 10.0.0.0/8 -x 192.168.0.0/16 $SUBNET &    
+    fi
+else
+    echo "Please, variable MODE only accepts 'genkey' or 'connect'."
+    exit 1
+fi
 
 _start
